f4141b8a1158ba4e0bc98fbef70b8259d6be4e30
o.yakovenko <alex231@ukr.net>
2024-04-19 19:54:11 +0200

    Add feed api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -9,3 +9,13 @@ class UpdateOwnProfile(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to edit their own profiles"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check if the user is the owner"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id
